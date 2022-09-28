f6e955b53967b1842dd030e690527870e05d765d
payal jaiswal <amitmetacoder@gmail.com>
2022-09-28 18:58:52 +0530

    28-09-2022

--- Dom_task/index.js
+++ Dom_task/index.js
@@ -5,30 +5,19 @@ function myfunction() {
 let arr = [];
 function sub(event) {
   event.preventDefault();
-  //   console.log("hrllo");
-  //   let x = document.forms["form1"];
-  //   console.log(x.value, "xxx");
-  //   if (x.value == "") {
-  //     alert("please fill all the field");
-  //   }
-  //   //   console.log(x[0].value, "value");
-  //   else {
-  //     let text = [];
-  //     for (let i = 0; i < x.length - 1; i++) {
-  //       text.push(x[i].value);
-  //     }
-  //     console.log(text);
-  //   }
-
-  let fname = document.getElementById("first_name").value;
-  let lname = document.getElementById("last_name").value;
-  let email = document.getElementById("email").value;
+  formvalidation();
+  let fname = document.getElementById("first_name").value.trim();
+  // if (fname == "") {
+  //   document.getElementById("msgfn").innerHTML = "firstname is required";
+  // }
+  let lname = document.getElementById("last_name").value.trim();
+  let email = document.getElementById("email").value.trim();
   let mobile = document.getElementById("mobile_no").value;
   console.log(lname, "lname");
   if (
+    fname == "" ||
     lname == "" ||
     lname == undefined ||
-    fname == "" ||
     email == "" ||
     mobile == ""
   ) {
@@ -50,90 +39,70 @@ function sub(event) {
                     <td>${arr[i].em}</td>
                     <td>${arr[i].mob}</td>
 
-                    <td><button class="btn btn-warning btn-sm" id="edit_data" onclick=edit(this)>Edit</button></td>
-                    
+                    <td><button class="btn btn-warning btn-sm" id="edit_data" onclick=edit(this)>Edit</button>
+                    <button class="btn btn-warning btn-sm" id="save_data" onclick=save(this)>Save</button></td>
                     <td><button class="btn btn-danger btn-sm" id="delete_data" onclick=remove(this)>Delete</button></td>`;
     }
     tables.appendChild(raw);
     console.log(arr, "arrAY");
-
-    // console.log("hello", fname);
-    // document.getElementById("fn").innerHTML = fname
-    // document.getElementById("ln").innerHTML = lname;
-    // document.getElementById("eml").innerHTML = email;
-    // document.getElementById("mob").innerHTML = mobile;
   }
 }
 
 function edit(e) {
-  // console.log("hello");
-  // e.parentElement.parentElement.remove();
-  // let tables = document.querySelector("table").querySelector("tbody");
-  // let raw = document.createElement("tr");
-  // for (let i = 0; i < arr.length; i++) {
-  //   raw.innerHTML = `<td><input value=${arr[i].fn}></td>
-  //                 <td><input value=${arr[i].ln}></td>
-  //                 <td><input value=${arr[i].em}></td>
-  //                <td> <input value=${arr[i].mob}></td>
-  //                 <td><button class="btn btn-warning btn-sm" id="edit_data" onclick=save(this)>save</button></td>
-  //                 <td><button class="btn btn-danger btn-sm" id="delete_data" onclick=remove(this)>Delete</button></td>`;
-  // }
-  // tables.appendChild(raw);
-  // let tr=
-  // document.getElementById("edit_data").style.display = "none";
-  // document.getElementById("save").style.display = "block";
-  // docu = e.parentElement.previousElementSibling.innerHTML;
-  // e.parentElement.parentElement.remove();
-  // let x = document.getElementById("edit_data").value;
+  e.setAttribute("style", "display:none;");
+  // document.getElementById("save_data").style.display = "block";
+  document.getElementById("submit").style.display = "none";
+  for (let i = 0; i < arr.length; i++) {
+    let fname = (document.querySelector("#first_name").value = arr[i].fn);
+    let lname = (document.querySelector("#last_name").value = arr[i].ln);
+    let email = (document.querySelector("#email").value = arr[i].em);
+    let mobile = (document.querySelector("#mobile_no").value = arr[i].mob);
+  }
+}
+
+function save(e) {
+  // document.getElementById("update").style.display = "none";
+  document.getElementById("submit").style.display = "block";
+  console.log("data is updated");
+  let raw = e.parentElement.parentElement;
+  let fname = document.getElementById("first_name").value;
+  let lname = document.getElementById("last_name").value;
+  let email = document.getElementById("email").value;
+  let mobile = document.getElementById("mobile_no").value;
+
+  raw.innerHTML = `<td>${fname}</td><td>${lname}</td><td>${email}</td><td>${mobile}</td><td><button class="btn btn-warning btn-sm" id="edit_data" onclick=edit(this)>Edit</button>
+  <button class="btn btn-warning btn-sm" id="save_data" onclick=save(this)>Save</button></td>
+  <td><button class="btn btn-danger btn-sm" id="delete_data" onclick=remove(this)>Delete</button></td>`;
+
+  let obj = { fn: fname, ln: lname, em: email, mob: mobile };
+  arr.splice(e.parentElement.parentElement, 1, obj);
+  console.log(arr);
+  document.forms[0].reset();
 }
 
 function remove(e) {
-  confirm("Sure you want to delete");
-  if (confirm == true) {
+  console.log(e.parentElement.parentElement);
+  if (confirm("Sure you want to delete")) {
     e.parentElement.parentElement.remove();
 
-    arr.splice(e.parentElement.parentElement.id, 1);
+    arr.splice(e.parentElement.parentElement, 1);
     console.log(arr);
   } else {
     console.log("dont delete");
   }
 }
 
-// for (let i = 0; i < arr.length; i++) {
-// document.getElementById("delete_data").parentElement.parentElement.remove();
-// arr[0].pop();
-// console.log(arr);
-// arr.pop()
-// console.log(arr)
-// }
+function formvalidation() {
+  let fn = document.getElementById("first_name").value;
 
-// if (document.querySelectorAll("tbody > tr")) {
-//   document
-//     .querySelectorAll("tbody > tr")
-//     [document.querySelectorAll("tbody > tr").length - 1].remove();
-//   arr.pop();
-//   console.log(arr);
-// }
-// var row = btn.parentNode.parentNode;
-// row.parentNode.removeChild(row);
-// // update your data (remove item by row id)
-// arr = arr.filter((item) => item.id !== `$(row).children()[0].innerText)`;
-// console.log(arr);
-// let deleteTask = (e) = {
+  let g = fn.trim();
+  console.log(g.length);
+  let em = document.getElementById("email").value;
 
-// localStorage.setItem("data", JSON.stringify(data));
-
-// $(function(){
-//   var arr= [
-//     {"name": "John"},
-//     {"name": "Henry"}
-//  ];
-//  $('.dm').click(function(){
-//       var val = $(this).closest('tr').find(".name").text();
-//       console.log(val);
-//       var index = arr.findIndex(function(item) {return item.name == val})
-//       console.log(index)
-//       arr.splice(index, 1)
-//       console.log(arr);
-//   })
-// })
+  if (g.length == "0") {
+    document.getElementById("msgfn").innerHTML = "First Name is Required";
+    return false;
+  }
+  //   if (em == n) {
+  //   }
+}
